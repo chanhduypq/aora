9de2c351e131ea16e14abc3291d2d881c903b876
chanhduypq <chanhduypq@gmail.com>
2018-03-12 16:07:45 +0700

    hiển thị comment phía dưới product trong url http://aora.localhost/result/id 3 file: crawlera-ca.crt, simple_html_dom.php, proxies.txt anh để folder và public luôn em nghe em thích để đâu đó thi để, anh nghĩ là tốt nhất để cả hai, để sau này minh có trỏ đến server.php hay public/index.php, thi khỏi mất công văng đưa ra đưa vô mấy cái file đó

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\Session\Session;
 
 class PagesController extends Controller
 {
+    public $proxyAuth = 'galvin24x7:egor99';
+    public $via_proxy = false;
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
@@ -83,7 +85,65 @@ class PagesController extends Controller
                 }
             }
         }
-        return view('pages.result', ['product' => $product]);
+        $review_html = $this->curl_execute($product->review_url);
+        
+        return view('pages.result', ['product' => $product, 'comments' => $this->getComments($review_html)]);
+    }
+    
+    private function getComments($content){
+        require_once 'simple_html_dom.php';
+        $html_base = new \simple_html_dom();
+        $html_base->load($content);
+        
+        $node = $html_base->find("#cm-cr-dp-review-list",0);
+        $comments = array();
+        $comment = array('avatar' => '', 'name' => '', 'time' => '', 'rating' => '', 'title' => '', 'title_url' => '', 'content' => '');
+        $nodes = $node->find("div[class='a-section review']");
+        foreach ($nodes as $node) {
+            $comment['avatar'] = $node->find(".a-profile-avatar", 0)->find('img', 1)->attr['src'];
+            $comment['name'] = $node->find('.a-profile-name', 0)->plaintext;
+            $comment['time'] = $node->find('span[data-hook="review-date"]', 0)->plaintext;
+            $comment['title'] = $node->find('a[data-hook="review-title"]', 0)->plaintext;
+            $comment['title_url'] = $node->find('a[data-hook="review-title"]', 0)->attr['href'];
+
+            $temp = $node->find("div[class='a-row']", 1);
+
+            $a = $temp->find('a', 0);
+            if($a->find("i", 0)){
+                $comment['rating_i_class'] = $a->find("i", 0)->attr['class'];
+                $comment['rating_i_html'] = $a->find("i", 0)->plaintext;
+            }
+            else{
+                $comment['rating_i_class'] = '';
+                $comment['rating_i_html'] = '';
+            }
+            
+            if(isset($a->attr['title'])){
+                $comment['rating_title'] = $a->attr['title'];
+            }
+            else{
+                $comment['rating_title'] = '';
+            }
+            
+            $comment['rating_href'] = $a->attr['href'];
+
+            $a = $temp->find('a[data-hook="review-title"]', 0);
+            if ($a) {
+                $comment['title_html'] = $a->plaintext;
+                $comment['title_href'] = $a->attr['href'];
+            } else {
+                $comment['title_html'] = '';
+                $comment['title_href'] = '';
+            }
+
+
+            list($comment['string1'], $comment['string2']) = explode("|", $node->find("div[class='a-row a-spacing-mini review-data review-format-strip']", 0)->plaintext);
+
+            $comment['content'] = $node->find('div[data-hook="review-collapsed"]', 0)->plaintext;
+            $comments[] = $comment;
+        }
+        
+        return $comments;
     }
 
     /**
@@ -102,4 +162,70 @@ class PagesController extends Controller
 
         die('done');
     }
+    
+
+    private function curl_execute($url, $json = false, $referer = false, $count = 0) {
+
+        $headers = array();
+        $headers[] = "Accept-Encoding: gzip, deflate";
+        $headers[] = "Accept-Language: en-US,en;q=0.9";
+        $headers[] = "Upgrade-Insecure-Requests: 1";
+        $headers[] = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36";
+        if ($json != false) {
+            $headers[] = "X-Requested-With: XMLHttpRequest";
+            $headers[] = "Accept: application/json";
+            $headers[] = "Content-Type: application/x-www-form-urlencoded";
+        } else {
+            $headers[] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+        }
+        $headers[] = "Cache-Control: max-age=0";
+        $headers[] = "Connection: keep-alive";
+        if ($referer != false) {
+            $headers[] = "Referer: " . $referer;
+        }
+
+        if (strrpos($url, 'shopee.sg/api/v1/items') !== false) {
+            $headers[] = "X-Csrftoken: Xg7jJJwJ4r6fcrtPDchtGilnpfaAB4YO";
+            $headers[] = "Cookie: _ga=GA1.2.680186883.1519700856; _gid=GA1.2.1946785702.1519700856; cto_lwid=6b9cee5e-f4f1-41c6-bdb7-9a3648cb988c; csrftoken=Xg7jJJwJ4r6fcrtPDchtGilnpfaAB4YO; __BWfp=c1519700860546xa2fcd5d59; SPC_IA=-1; SPC_U=-; SPC_EC=-; bannerShown=true; SPC_SC_TK=; UYOMAPJWEMDGJ=; SPC_SC_UD=; SPC_F=zRjUetudjMWMgmUmr3f8Tij0vF6L3p0I; REC_T_ID=5c6c2e48-1b6b-11e8-9b1a-1866dab29c0a; SPC_T_ID=\"99jVmjgK9KZL0SnPMX/yuwLLv9M3sEGDo+J3VZT9ZSQx3lifdMmK2MmdqjtqdRttt3ZgPL+lyYVOwmvMZ1z5kZsGi/X9Sfz54Vps8e6Eq1w=\"; SPC_SI=qqdhd4n2jlz4i9124ah7o2wr8m4gaafz; SPC_T_IV=\"Eqx+GcOke9cn5Sl3jATR4A==\"; _gat=1";
+        }
+
+
+        $ch = curl_init();
+
+        //check use proxies
+        if ($this->via_proxy) {
+            curl_setopt($ch, CURLOPT_PROXY, 'http://' . $this->getProxy());
+            curl_setopt($ch, CURLOPT_PROXYUSERPWD, $this->proxyAuth);
+        }
+        //post json data
+        if ($json != false) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+            curl_setopt($ch, CURLOPT_POST, 1);
+        } else {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        }
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+        $content = curl_exec($ch);
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        // print_r("status: ".$status."\n");
+
+        if (($status != 200 && $status != 404) || trim($content) == '' || (strpos($url, 'abc') !== false && strpos($content, '</html>') !== false) && $count < 4) {
+            sleep(1);
+            $count++;
+            return $this->curl_execute($url, $json, $referer, $count);
+        }
+
+        return $content;
+    }
+    private function getProxy() {
+        $f_contents = file("proxies.txt");
+        $line = trim($f_contents[rand(0, count($f_contents) - 1)]);
+        return $line;
+    }
+
 }
--- resources/views/pages/result.blade.php
+++ resources/views/pages/result.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+<link rel="stylesheet" href="https://images-na.ssl-images-amazon.com/images/I/61P4yv5CcRL._RC|01evdoiemkL.css,01K+Ps1DeEL.css,31yErFkQitL.css,01kivkxD60L.css,11UGC+GXOPL.css,21LK7jaicML.css,11L58Qpo0GL.css,21EuGTxgpoL.css,01Xl9KigtzL.css,21IJTTf5-5L.css,019SHZnt8RL.css,01qy9K8SDEL.css,11vZhCgAHbL.css,21uiGhnhrlL.css,11WgRxUdJRL.css,01dU8+SPlFL.css,11ocrgKoE-L.css,01SHjPML6tL.css,111-D2qRjiL.css,01QrWuRrZ-L.css,31jVVYSnroL.css,114KWZGKCVL.css,01Alnvtt1zL.css,01oZl+VEzRL.css_.css?AUIClients/AmazonUI#us.not-trident" />
         <div class="page search page--padding-bottom">
             <div class="container">
                 <div class="search-body">
@@ -66,6 +67,48 @@
                             <input type="hidden" name="shipping_dimension" value="{{ $product->dimensionsAsString }}" id="product-dimensions"/>
                             {{ csrf_field() }}
                         </form>
+                    </div>
+                    <div class="item d-md-flex" style="margin-top: 30px;">
+                        <div id="cm-cr-dp-review-list" class="a-section review-views celwidget" data-hook="top-customer-reviews-widget">
+                            @foreach ($comments as $comment) 
+                            <div class="a-section review">
+                                <div class="a-section celwidget">
+                                    <div class="a-row a-spacing-mini">
+                                        <div aria-hidden="true" class="a-profile-avatar-wrapper">
+                                            <div class="a-profile-avatar">
+                                                <img src="{{ $comment['avatar'] }}" class="" data-src="{{ $comment['avatar'] }}">
+                                            </div>
+                                        </div>
+                                        <div class="a-profile-content"><span class="a-profile-name">{{ $comment['name'] }}</span></div>
+
+                                    </div>
+                                    <div class="a-row">
+                                        <a class="a-link-normal" title="{{ $comment['rating_title'] }}" href="{{ $comment['rating_href'] }}">
+                                            <i data-hook="review-star-rating" class="{{ $comment['rating_i_class'] }}"><span class="a-icon-alt">{{ $comment['rating_i_html'] }}</span></i>
+                                        </a>
+                                        <span class="a-letter-space"></span>
+                                        <a data-hook="review-title" class="a-size-base a-link-normal review-title a-color-base a-text-bold" href="{{ $comment['title_href'] }}">{{ $comment['title_html'] }}</a>
+                                    </div>
+                                    <span data-hook="review-date" class="a-size-base a-color-secondary review-date">{{ $comment['time'] }}</span>
+                                    <div class="a-row a-spacing-mini review-data review-format-strip">
+                                        <span data-hook="format-strip-linkless" class="a-color-secondary">
+                                            {{ $comment['string1'] }}<i class="a-icon a-icon-text-separator" aria-label="|"><span class="a-icon-alt">|</span></i>{{ $comment['string2'] }}
+                                        </span>
+                                    </div>
+                                    <div class="a-row review-data">
+                                        <span data-hook="review-body" class="a-size-base review-text">
+                                            <div aria-live="polite" data-a-expander-name="review_text_read_more" data-a-expander-collapsed-height="300" class="a-expander-collapsed-height a-row a-expander-container a-spacing-base a-expander-partial-collapse-container" style="max-height:300px; _height:300px">
+                                                <div data-hook="review-collapsed" aria-expanded="false" class="a-expander-content a-expander-partial-collapse-content">
+                                                    {{ $comment['content'] }}
+                                                </div>
+                                                <div class="a-expander-header a-expander-partial-collapse-header" style="opacity: 0; display: none;"><div class="a-expander-content-fade"></div><a href="javascript:void(0)" data-hook="expand-collapse-read-more-less" data-action="a-expander-toggle" class="a-declarative" data-a-expander-toggle="{&quot;allowLinkDefault&quot;:true, &quot;expand_prompt&quot;:&quot;Read more&quot;, &quot;collapse_prompt&quot;:&quot;Read less&quot;}"><i class="a-icon a-icon-extender-expand"></i><span class="a-expander-prompt">Read more</span></a></div>
+                                            </div>                                        
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
                     </div>
                     @else
                         <p>Nothing find</p>
